36b3bc913b77ef045f95864083c6e38488257a5f
John Bower <john@industrialassets.co.uk>
2024-12-12 15:45:18 +0000

    added missing use code array

--- src/helpers/seed-db/data/actions.test.js
+++ src/helpers/seed-db/data/actions.test.js
@@ -18,4 +18,33 @@ describe('populateClasses', () => {
     expect(populatedData[0].uses.includes('AC10')).toBeTruthy()
     expect(populatedData[0].uses).toHaveLength(101)
   })
+
+  test('should work with multiple actions', () => {
+    const populatedData = populateActionClasses([
+      {
+        code: 'TMP1',
+        description: 'This is a made up action',
+        uses: ['PG01', 'AC10'],
+        payment: {
+          amountPerHectare: 5.8,
+          additionalPaymentPerAgreement: 95
+        },
+        eligibilityRules: []
+      },
+      {
+        code: 'TMP2',
+        description: 'This is a made up action',
+        uses: ['PG01', 'AC10'],
+        payment: {
+          amountPerHectare: 5.8,
+          additionalPaymentPerAgreement: 95
+        },
+        eligibilityRules: []
+      }
+    ])
+
+    expect(populatedData[0].uses.includes('AC10')).toBeTruthy()
+    expect(populatedData[1].uses.includes('AC10')).toBeTruthy()
+    expect(populatedData).toHaveLength(2)
+  })
 })
