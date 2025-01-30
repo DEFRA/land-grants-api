f8a8521eed101a81d15bdfe61bc8a2fc862da2d9
Bill Sclater <bill.sclater@equalexperts.com>
2025-01-30 11:37:44 +0000

    changed source code to poc

--- src/api/router.js
+++ src/api/router.js
@@ -1,9 +1,5 @@
 import { health } from '~/src/api/health/index.js'
-import { compatibilityMatrix } from '~/src/api/compatibility-matrix/index.js'
-import { action } from '~/src/api/action/index.js'
-import { land } from '~/src/api/land/index.js'
-import { availableArea } from '~/src/api/available-area/index.js'
-import { paymentCalculation } from '~/src/api/payment/index.js'
+import { example } from '~/src/api/example/index.js'
 
 /**
  * @satisfies { import('@hapi/hapi').ServerRegisterPluginObject<*> }
@@ -15,20 +11,8 @@ const router = {
       // Health-check route. Used by platform to check if service is running, do not remove!
       await server.register([health])
 
-      // Get Action compatibility matrix
-      await server.register([compatibilityMatrix])
-
-      // Get Actions
-      await server.register([action])
-
-      // Get Available Area
-      await server.register([availableArea])
-
-      // Land - Parcels, Cover
-      await server.register([land])
-
-      // Payment Calculation
-      await server.register([paymentCalculation])
+      // Application specific routes, add your own routes here.
+      await server.register([example])
     }
   }
 }
