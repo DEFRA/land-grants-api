fc6c7b2bde42014a1f730639c5efe3e9cbded34b
Alistair Laing <aliuk2012@users.noreply.github.com>
2025-04-28 13:45:31 +0100

    Fix 'magic number' issue identified by sonarcloud

--- src/api/common/schema/index.js
+++ src/api/common/schema/index.js
@@ -1,4 +1,5 @@
 import Joi from 'joi'
+import { statusCodes } from '~/src/api/common/constants/status-codes.js'
 
 /**
  * @typedef {object} ErrorResponseSchema
@@ -7,7 +8,7 @@ import Joi from 'joi'
  * @property {string} message
  */
 const errorResponseSchema = Joi.object({
-  statusCode: Joi.number().valid(404).required(),
+  statusCode: Joi.number().valid(statusCodes.notFound).required(),
   error: Joi.string().required(),
   message: Joi.string().required()
 })
@@ -19,7 +20,7 @@ const errorResponseSchema = Joi.object({
  * @property {string} message
  */
 const internalServerErrorResponseSchema = Joi.object({
-  statusCode: Joi.number().valid(500).required(),
+  statusCode: Joi.number().valid(statusCodes.internalServerError).required(),
   error: Joi.string().required(),
   message: Joi.string().required()
 })
