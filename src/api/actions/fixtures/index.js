2c02ee5a610fd71d16cc09b7c2ba4b78be501ee6
andrewkeig <andrew.keig@gmail.com>
2025-05-14 14:41:53 +0100

    Update action schema

--- src/api/actions/fixtures/index.js
+++ src/api/actions/fixtures/index.js
@@ -1,12 +1,76 @@
+// const mockActions = [
+//   {
+//     code: 'CSAM1',
+//     description:
+//       'CSAM1: Assess soil, produce a soil management plan and test soil organic matter',
+//   }
+// ]
+
 const mockActions = [
   {
-    code: 'CSAM1',
-    description:
-      'CSAM1: Assess soil, produce a soil management plan and test soil organic matter',
-    availableArea: {
-      unit: 'ha',
-      value: 0
-    }
+    version: 1,
+    startDate: '2025-01-01',
+    code: 'CMOR1',
+    description: 'CMOR1: Assess moorland and produce a written record',
+    applicationUnitOfMeasurement: 'ha',
+    payment: {
+      ratePerUnitGbp: 10.6,
+      ratePerAgreementPerYearGbp: 272
+    },
+    landCoverCodes: [
+      '131',
+      '241',
+      '243',
+      '251',
+      '252',
+      '253',
+      '271',
+      '281',
+      '282',
+      '283',
+      '285',
+      '286',
+      '287',
+      '288',
+      '300',
+      '347',
+      '582',
+      '583',
+      '591',
+      '592',
+      '593',
+      '621',
+      '641',
+      '643',
+      '651'
+    ],
+    landCoverClassCodes: [
+      '130',
+      '240',
+      '250',
+      '270',
+      '280',
+      '300',
+      '330',
+      '580',
+      '590',
+      '620',
+      '640',
+      '650'
+    ],
+    rules: [
+      {
+        name: 'parcel-has-intersection-with-data-layer',
+        config: {
+          layerName: 'moorland',
+          minimumIntersectionPercent: 50,
+          tolerancePercent: 1
+        }
+      },
+      {
+        name: 'applied-for-total-available-area'
+      }
+    ]
   }
 ]
 
