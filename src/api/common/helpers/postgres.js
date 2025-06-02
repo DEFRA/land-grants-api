8ff89ccce8b6ea565023f593295bdeaddeedbdc0
David Capilla <david.capilla@gmail.com>
2025-06-02 14:19:44 +0100

    New approach to dynamically regenerate tokens on postgres connect

--- src/api/common/helpers/postgres.js
+++ src/api/common/helpers/postgres.js
@@ -1,6 +1,6 @@
 import { fromNodeProviderChain } from '@aws-sdk/credential-providers'
 import { Signer } from '@aws-sdk/rds-signer'
-import { Client, Pool } from 'pg'
+import { Pool } from 'pg'
 import { config } from '~/src/config/index.js'
 import { loadPostgresData } from './load-land-data.js'
 
@@ -27,72 +27,6 @@ async function getToken(options) {
 }
 
 /**
- * Custom Client class that fetches fresh IAM tokens on connect
- */
-class IAMClient extends Client {
-  constructor(config, server, options) {
-    super(config)
-    this.server = server
-    this.options = options
-  }
-
-  async connect() {
-    try {
-      this.server.logger.info('IAMClient connecting with fresh token')
-      const password = await getToken(this.options)
-
-      // Update the connectionParameters.password which is what pg uses
-      this.connectionParameters.password = password
-      this.password = password
-
-      this.server.logger.info('Password set for connection')
-      return super.connect()
-    } catch (err) {
-      this.server.logger.error({ err }, 'Failed to connect with IAM token')
-      throw err
-    }
-  }
-}
-
-/**
- * Custom Pool class using IAMClient
- */
-class SecurePool extends Pool {
-  constructor(options, server) {
-    server.logger.info(
-      `Creating SecurePool with options: ${JSON.stringify({
-        ...options,
-        passwordForLocalDev: options.passwordForLocalDev
-          ? '[REDACTED]'
-          : undefined
-      })}`
-    )
-
-    super({
-      host: options.host,
-      port: options.port,
-      database: options.database,
-      user: options.user,
-      region: options.region,
-      Client: class extends IAMClient {
-        constructor(config) {
-          super(config, server, options)
-        }
-      },
-      ssl: !options.isLocal
-        ? {
-            rejectUnauthorized: false,
-            secureContext: server.secureContext
-          }
-        : undefined
-    })
-
-    this.options = options
-    this.server = server
-  }
-}
-
-/**
  * @satisfies { import('@hapi/hapi').ServerRegisterPluginObject<*> }
  */
 export const postgresDb = {
@@ -113,18 +47,13 @@ export const postgresDb = {
         return
       }
 
-      const pool = new SecurePool(
-        {
-          port: DEFAULT_PORT,
-          user: options.user,
-          host: options.host,
-          database: options.database,
-          region: options.region,
-          passwordForLocalDev: options.passwordForLocalDev,
-          isLocal: options.isLocal
-        },
-        server
-      )
+      const pool = new Pool({
+        port: DEFAULT_PORT,
+        user: options.user,
+        host: options.host,
+        database: options.database,
+        password: async () => await getToken(options)
+      })
 
       try {
         const client = await pool.connect()
