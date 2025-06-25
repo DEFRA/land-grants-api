13e257a3f9c8020aad6f37ed56adbc7892b138f2
andrewkeig <andrew.keig@gmail.com>
2025-06-25 16:51:23 +0100

    cleanup the tests

--- src/db-tests/availableArea.test.js
+++ src/db-tests/availableArea.test.js
@@ -1,41 +1,34 @@
-import mongoose from 'mongoose'
 import compatibilityMatrixModel from '~/src/api/actions/models/compatibilityMatrix.model.js'
 import { calculateAvailableArea } from '~/src/available-area/index.js'
 import compatibilityMatrix from '~/src/db-tests/fixtures/compatibility-matrix.json'
 import { compatibilitycodes } from '~/src/db-tests/fixtures/compatibilityCodes.js'
+import {
+  connectMongo,
+  seedMongo,
+  closeMongo
+} from '~/src/db-tests/setup/utils.js'
 
 const logger = {
   info: jest.fn(),
   error: jest.fn()
 }
 
-describe('Get land cover codes', () => {
+describe('Calculate available area', () => {
   beforeAll(async () => {
-    await mongoose.connect(
-      `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}`,
-      {
-        dbName: 'land-grants-api'
-      }
+    await connectMongo()
+    await seedMongo(
+      compatibilityMatrixModel,
+      'compatibility-matrix',
+      compatibilityMatrix
     )
-
-    try {
-      await compatibilityMatrixModel.db.dropCollection('compatibility-matrix')
-    } catch (error) {
-      // Ignore error if collection doesn't exist
-      if (error.codeName !== 'NamespaceNotFound') {
-        throw error
-      }
-    }
-    await compatibilityMatrixModel.insertMany(compatibilityMatrix)
   })
 
   afterAll(async () => {
-    await mongoose.disconnect()
+    await closeMongo()
   })
 
   test('should return availableArea', async () => {
     const landCovers = await calculateAvailableArea(logger)
     expect(landCovers).toEqual(compatibilitycodes)
-    expect(true).toBe(true)
   })
 })
--- src/db-tests/getCompatibilityMatrix.query.js
+++ src/db-tests/getCompatibilityMatrix.query.js
@@ -1,41 +1,34 @@
-import mongoose from 'mongoose'
 import compatibilityMatrixModel from '~/src/api/actions/models/compatibilityMatrix.model.js'
 import { getCompatibilityMatrix } from '~/src/api/compatibility-matrix/queries/getCompatibilityMatrix.query.js'
 import compatibilityMatrix from '~/src/db-tests/fixtures/compatibility-matrix.json'
 import { compatibilitycodes } from '~/src/db-tests/fixtures/compatibilityCodes.js'
+import {
+  connectMongo,
+  seedMongo,
+  closeMongo
+} from '~/src/db-tests/setup/utils.js'
 
 const logger = {
   info: jest.fn(),
   error: jest.fn()
 }
 
-describe('Get land cover codes', () => {
+describe('Get compatibility matrix', () => {
   beforeAll(async () => {
-    await mongoose.connect(
-      `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}`,
-      {
-        dbName: 'land-grants-api'
-      }
+    await connectMongo()
+    await seedMongo(
+      compatibilityMatrixModel,
+      'compatibility-matrix',
+      compatibilityMatrix
     )
-
-    try {
-      await compatibilityMatrixModel.db.dropCollection('compatibility-matrix')
-    } catch (error) {
-      // Ignore error if collection doesn't exist
-      if (error.codeName !== 'NamespaceNotFound') {
-        throw error
-      }
-    }
-    await compatibilityMatrixModel.insertMany(compatibilityMatrix)
   })
 
   afterAll(async () => {
-    await mongoose.disconnect()
+    await closeMongo()
   })
 
-  test('should return all optionCodeCompat for code', async () => {
+  test('should return all optionCodes for code', async () => {
     const landCovers = await getCompatibilityMatrix(['CMOR1'], logger)
     expect(landCovers).toEqual(compatibilitycodes)
-    expect(true).toBe(true)
   })
 })
