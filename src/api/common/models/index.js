716f54672c6a71ee741f6877dc97dea11ecc38e2
andrewkeig <andrew.keig@gmail.com>
2025-07-09 19:25:17 +0100

    remove dead code, truncate data on import

--- src/api/common/models/index.js
+++ src/api/common/models/index.js
@@ -1,9 +1,7 @@
 import actionData from '../../actions/models/action.model.js'
-import landCoverCodes from '../../land-cover-codes/models/land-cover-codes.model.js'
 import compatibilityMatrix from '../../compatibility-matrix/models/compatibilityMatrix.model.js'
 
 export default {
   'action-data': actionData,
-  'land-cover-codes': landCoverCodes,
   'compatibility-matrix': compatibilityMatrix
 }
