5efd97a48ba282ab0069d851c7374391998a0b5b
Andrew Timney <andrew.timney@gmail.com>
2025-07-17 17:00:42 +0100

    transform compatibility matrix to camelCase

--- src/api/compatibility-matrix/compatibility-matrix.d.js
+++ src/api/compatibility-matrix/compatibility-matrix.d.js
@@ -1,7 +1,7 @@
 /**
  * @typedef {object} CompatibilityMatrix
  * @property {number} id
- * @property {string} option_code
- * @property {string} option_code_compat
+ * @property {string} optionCode
+ * @property {string} optionCodeCompat
  * @property {number} year
  */
--- src/api/compatibility-matrix/queries/getCompatibilityMatrix.query.test.js
+++ src/api/compatibility-matrix/queries/getCompatibilityMatrix.query.test.js
@@ -1,5 +1,6 @@
 import { jest } from '@jest/globals'
 import { getCompatibilityMatrix } from '~/src/api/compatibility-matrix/queries/getCompatibilityMatrix.query.js'
+import { compatibilityMatrixTransformer } from '../compatibility-matrix.transformer.js'
 
 describe('getCompatibilityMatrix', () => {
   const mockLogger = {
@@ -7,7 +8,12 @@ describe('getCompatibilityMatrix', () => {
     info: jest.fn()
   }
   const queryMock = jest.fn()
-  const cm = { option_code: 'AFC', option_code_compat: 'ABC', year: 2019 }
+  const cm = {
+    id: 1,
+    option_code: 'AFC',
+    option_code_compat: 'ABC',
+    year: 2019
+  }
   queryMock.mockResolvedValue({
     rows: [cm]
   })
@@ -26,7 +32,7 @@ describe('getCompatibilityMatrix', () => {
   test('should select all compatibility matrix', async () => {
     const result = await getCompatibilityMatrix(mockLogger, dbMock)
 
-    expect(result).toEqual([cm])
+    expect(result).toEqual([compatibilityMatrixTransformer(cm)])
     expect(dbMock.connect).toHaveBeenCalled()
     expect(queryMock).toHaveBeenCalledWith(
       'SELECT * FROM compatibility_matrix ',
@@ -41,7 +47,7 @@ describe('getCompatibilityMatrix', () => {
     const codes = ['CMOR1']
     const result = await getCompatibilityMatrix(mockLogger, dbMock, codes)
 
-    expect(result).toEqual([cm])
+    expect(result).toEqual([compatibilityMatrixTransformer(cm)])
     expect(dbMock.connect).toHaveBeenCalled()
     expect(queryMock).toHaveBeenCalledWith(
       'SELECT * FROM compatibility_matrix WHERE option_code = ANY ($1)',
