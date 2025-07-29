24b16f2d11dcd814433c6e2b07458c8034a32a11
David Capilla <david.capilla@gmail.com>
2025-07-29 11:08:28 +0100

    Changes to generateSchedule and payment calculation to create dates array

--- src/payment-calculation/generateSchedule.test.js
+++ src/payment-calculation/generateSchedule.test.js
@@ -1,39 +1,70 @@
-import { addMonths, addYears, format, startOfMonth } from 'date-fns'
-import {
-  generatePaymentSchedule,
-  SCHEDULE_DATE_FORMAT
-} from './generateSchedule.js'
+import { generatePaymentSchedule } from './generateSchedule.js'
 
 describe('generateSchedule', () => {
+  test('returns empty schedule if no lengthYears is specified', () => {
+    jest.useFakeTimers().setSystemTime(new Date(2025, 6, 2))
+    const today = new Date()
+
+    const { schedule } = generatePaymentSchedule(today)
+
+    expect(schedule).toEqual([])
+  })
+
+  test('returns empty schedule if lengthYears is not a number', () => {
+    jest.useFakeTimers().setSystemTime(new Date(2025, 6, 2))
+    const today = new Date()
+
+    const { schedule } = generatePaymentSchedule(today, 'lengthInYears')
+
+    expect(schedule).toEqual([])
+  })
+
   test('returns quarterly payment dates', () => {
     jest.useFakeTimers().setSystemTime(new Date(2025, 6, 2))
 
     const lengthYears = 3
+    const frequency = 'quarterly'
     const today = new Date()
-    const { agreementStartDate, agreementEndDate } = generatePaymentSchedule(
-      today,
-      lengthYears
-    )
+    const { agreementStartDate, agreementEndDate, schedule } =
+      generatePaymentSchedule(today, lengthYears, frequency)
 
-    expect(agreementStartDate).toEqual(
-      format(startOfMonth(addMonths(today, 1)), SCHEDULE_DATE_FORMAT)
-    )
-    expect(agreementEndDate).toEqual(
-      format(addYears(agreementStartDate, 3), SCHEDULE_DATE_FORMAT)
-    )
-    // expect(schedule).toEqual([
-    //   new Date(2025, 10, 5),
-    //   new Date(2026, 1, 5),
-    //   new Date(2026, 4, 5),
-    //   new Date(2026, 7, 5),
-    //   new Date(2026, 10, 5),
-    //   new Date(2027, 1, 5),
-    //   new Date(2027, 4, 5),
-    //   new Date(2027, 7, 5),
-    //   new Date(2027, 10, 5),
-    //   new Date(2028, 1, 7),
-    //   new Date(2028, 4, 5),
-    //   new Date(2028, 7, 7)
-    // ])
+    expect(agreementStartDate).toBe('2025-08-01')
+    expect(agreementEndDate).toBe('2028-08-01')
+
+    expect(schedule).toEqual([
+      '2025-11-05',
+      '2026-02-05',
+      '2026-05-05',
+      '2026-08-05',
+      '2026-11-05',
+      '2027-02-05',
+      '2027-05-05',
+      '2027-08-05',
+      '2027-11-05',
+      '2028-02-07',
+      '2028-05-05',
+      '2028-08-07'
+    ])
+  })
+
+  test('returns yearly payment dates', () => {
+    jest.useFakeTimers().setSystemTime(new Date(2025, 6, 2))
+
+    const lengthYears = 5
+    const frequency = 'yearly'
+    const today = new Date()
+    const { agreementStartDate, agreementEndDate, schedule } =
+      generatePaymentSchedule(today, lengthYears, frequency)
+
+    expect(agreementStartDate).toBe('2025-08-01')
+    expect(agreementEndDate).toBe('2030-08-01')
+
+    expect(schedule).toEqual([
+      '2026-08-05',
+      '2027-08-05',
+      '2028-08-07',
+      '2029-08-06',
+      '2030-08-05'
+    ])
   })
 })
