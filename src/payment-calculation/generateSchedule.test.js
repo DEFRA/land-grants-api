75e23902ff3151f8a839efad93eea3313f4f4a46
David Capilla <david.capilla@gmail.com>
2025-07-29 11:36:17 +0100

    Further testing on generateSchedule

--- src/payment-calculation/generateSchedule.test.js
+++ src/payment-calculation/generateSchedule.test.js
@@ -1,7 +1,7 @@
 import { generatePaymentSchedule } from './generateSchedule.js'
 
 describe('generateSchedule', () => {
-  test('returns empty schedule if no lengthYears is specified', () => {
+  it('should return an empty schedule array if no lengthYears is specified', () => {
     jest.useFakeTimers().setSystemTime(new Date(2025, 6, 2))
     const today = new Date()
 
@@ -10,7 +10,7 @@ describe('generateSchedule', () => {
     expect(schedule).toEqual([])
   })
 
-  test('returns empty schedule if lengthYears is not a number', () => {
+  it('should return an empty schedule array if lengthYears is not a number', () => {
     jest.useFakeTimers().setSystemTime(new Date(2025, 6, 2))
     const today = new Date()
 
@@ -19,7 +19,7 @@ describe('generateSchedule', () => {
     expect(schedule).toEqual([])
   })
 
-  test('returns quarterly payment dates', () => {
+  it('should return a schedule of payment dates for quarterly frequency for 3 years', () => {
     jest.useFakeTimers().setSystemTime(new Date(2025, 6, 2))
 
     const lengthYears = 3
@@ -47,7 +47,7 @@ describe('generateSchedule', () => {
     ])
   })
 
-  test('returns yearly payment dates', () => {
+  it('should return a schedule of payment dates for yearly frequency for 5 years', () => {
     jest.useFakeTimers().setSystemTime(new Date(2025, 6, 2))
 
     const lengthYears = 5
@@ -65,6 +65,34 @@ describe('generateSchedule', () => {
       '2028-08-07',
       '2029-08-06',
       '2030-08-05'
+    ])
+  })
+
+  it('should return a schedule of payment dates for monthly frequency for 1 year', () => {
+    jest.useFakeTimers().setSystemTime(new Date(2025, 6, 2))
+
+    const lengthYears = 1
+    const frequency = 'monthly'
+    const today = new Date()
+    const { agreementStartDate, agreementEndDate, schedule } =
+      generatePaymentSchedule(today, lengthYears, frequency)
+
+    expect(agreementStartDate).toBe('2025-08-01')
+    expect(agreementEndDate).toBe('2026-08-01')
+
+    expect(schedule).toEqual([
+      '2025-09-05',
+      '2025-10-06',
+      '2025-11-05',
+      '2025-12-05',
+      '2026-01-05',
+      '2026-02-05',
+      '2026-03-05',
+      '2026-04-06',
+      '2026-05-05',
+      '2026-06-05',
+      '2026-07-06',
+      '2026-08-05'
     ])
   })
 })
