54a33e2675749a63caaaa7dd374414d09896d4ad
andrewkeig <andrew.keig@gmail.com>
2025-09-12 13:10:15 +0100

    add get application validation run controller

--- src/api/application/schema/application-validation.schema.js
+++ src/api/application/schema/application-validation.schema.js
@@ -23,4 +23,27 @@ const applicationValidationSchema = Joi.object({
     .required()
 })
 
-export { applicationValidationSchema }
+const applicationValidationRunSchema = Joi.object({
+  id: Joi.number().integer().required(),
+  application_id: Joi.string().required(),
+  sbi: Joi.string().required(),
+  crn: Joi.string().required(),
+  data: Joi.object().required(),
+  created_at: Joi.date().required()
+})
+
+const applicationValidationRunRequestSchema = Joi.object({
+  id: Joi.number().integer().required()
+})
+
+const applicationValidationRunResponseSchema = Joi.object({
+  message: Joi.string().required(),
+  applicationValidationRun: applicationValidationRunSchema
+})
+
+export {
+  applicationValidationSchema,
+  applicationValidationRunSchema,
+  applicationValidationRunRequestSchema,
+  applicationValidationRunResponseSchema
+}
