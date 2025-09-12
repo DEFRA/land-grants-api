2d3db70f084321102d932ac550bbaf7dfd58c4ca
Andrew Timney <andrew.timney@gmail.com>
2025-09-12 14:20:00 -0100

    Feat/SFIR-374: Create application object (#195)
    * create application data object
    
    * return application run id
    
    * use status code const

--- src/api/application/application.d.js
+++ src/api/application/application.d.js
@@ -1,9 +1,62 @@
 /**
+ * @typedef {object} RuleResult
+ * @property {boolean} hasPassed
+ * @property {string} name
+ * @property {string} reason
+ * @property {object[]} explanations
+ */
+
+/**
+ * @typedef {object} ActionResult
+ * @property {boolean} hasPassed
+ * @property {string} code
+ * @property {string} actionConfigVersion
+ * @property {object[]} rules
+ */
+
+/**
+ * @typedef {object} ParcelResult
+ * @property {string} sheetId
+ * @property {string} parcelId
+ * @property {ActionResult[]} actions
+ */
+
+/**
+ * @typedef {object} Action
+ * @property {string} code
+ * @property {number} quantity
+ */
+
+/**
+ * @typedef {object} Parcel
+ * @property {string} sheetId
+ * @property {string} parcelId
+ * @property {Action[]} actions
+ */
+
+/**
+ * @typedef {object} Application
+ * @property {string} applicantCrn
+ * @property {Parcel[]} parcels
+ */
+
+/**
+ * @typedef {object} ApplicationResultData
+ * @property {Date} date
+ * @property {string} requester
+ * @property {string} landGrantsApiVersion
+ * @property {boolean} hasPassed
+ * @property {Application} application
+ * @property {ParcelResult[]} parcelLevelResults
+ * @property {Date} created_at
+ */
+
+/**
  * @typedef {object} ApplicationResult
  * @property {number} id
  * @property {string} application_id
  * @property {string} sbi
  * @property {string} crn
- * @property {object} data
+ * @property {ApplicationResultData} data
  * @property {Date} created_at
  */
