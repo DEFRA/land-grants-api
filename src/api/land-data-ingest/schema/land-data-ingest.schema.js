a4461a0de799b36f6577195e37d9b738b4e1e996
Andrew Timney <andrew.timney@gmail.com>
2025-10-22 14:23:17 +0100

    add endpoint to handle cdp-uploader events

---
+++ src/api/land-data-ingest/schema/land-data-ingest.schema.js
@@ -0,0 +1,30 @@
+import Joi from 'joi'
+
+const cdpUploaderCallbackSchema = Joi.object({
+  uploadStatus: Joi.string().valid('initiated', 'pending', 'ready').required(),
+  numberOfRejectedFiles: Joi.number().integer(),
+  metadata: Joi.object({
+    customerId: Joi.string(),
+    accountId: Joi.string()
+  }),
+  form: Joi.object({
+    file: Joi.object({
+      fileId: Joi.string().required(),
+      filename: Joi.string().required(),
+      contentType: Joi.string().required(),
+      fileStatus: Joi.string()
+        .valid('pending', 'complete', 'rejected')
+        .required(),
+      contentLength: Joi.number().integer().required(),
+      checksumSha256: Joi.string().required(),
+      s3Key: Joi.string().required(),
+      s3Bucket: Joi.string().required()
+    }).required()
+  }).required()
+})
+
+const cdpUploaderCallbackResponseSchema = Joi.object({
+  message: Joi.string().required()
+}).required()
+
+export { cdpUploaderCallbackSchema, cdpUploaderCallbackResponseSchema }
