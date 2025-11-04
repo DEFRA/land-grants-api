ed8ecdeb3112d7496f5162226dd66897e43012bd
David Middleton <david.middleton@esynergy.co.uk>
2025-11-04 11:13:46 +0000

    Update compose and npm run scripts

--- README.md
+++ README.md
@@ -132,6 +132,30 @@ To run the application in `development` mode run the following command:
 npm run dev
 ```
 
+Or via Docker compose:
+
+```bash
+# Run the API and Postgres in Docker without Localstack
+npm run docker:up
+```
+
+```bash
+# Stop the API and Postgres containers
+npm run docker:down
+```
+
+```bash
+# Run the API, Postgres and Localstack in Docker
+npm run docker:localstack:up
+```
+
+```bash
+# Stop the API, Postgres and Localstack containers
+npm run docker:localstack:down
+```
+
+Note: Running via Docker compose supports hot reloading of the API code, so you can make changes to the code and see them reflected in the running API.
+
 ### Database migrations with Docker Compose
 
 This project uses Liquibase to manage database schema changes. There are two ways to run migrations locally, depending on whether you want to run them inside the main `compose.yml` stack or against an existing database from a separate, `grants-ui` compose file.
@@ -152,9 +176,13 @@ npm run docker:up
 Once the database is healthy, you may run the migration containers within the same stack if required:
 
 ```bash
-docker compose --profile migrations run --rm database-up
-# or to roll back
-docker compose --profile migrations run --rm database-down
+npm run docker:migrate:up
+```
+
+And optionally run the "down" migrations:
+
+```bash
+npm run docker:migrate:down
 ```
 
 Bring the stack down when you’re done:
@@ -176,15 +204,15 @@ Convenient npm scripts have been added for this workflow:
 
 ```bash
 # Apply migrations to the grants-ui database
-npm run docker:db:migrate:up
+npm run docker:migrate:ext:up
+```
 
+```bash
 # Roll back all migrations to the base tag v0.0.0
-npm run docker:db:migrate:down
+npm run docker:migrate:ext:down
 ```
 
 ### Testing
-
-In order to run the `db tests` please make sure you have `docker` running
 
 In order to run the test(s)
 
--- src/db-tests/setup/jestSetup.js
+++ src/db-tests/setup/jestSetup.js
@@ -1,78 +1,8 @@
-import { type } from 'os'
-import path from 'path'
-import { fileURLToPath } from 'url'
-import { GenericContainer, Network, Wait } from 'testcontainers'
 import { config } from '../../config/index.js'
-
-export const containers = []
 
 export const DB_CONFIG = {
   host: config.get('postgres.host'),
   user: config.get('postgres.user'),
   database: config.get('postgres.database'),
   password: config.get('postgres.passwordForLocalDev')
-}
-
-const log = (stream, name) => {
-  /* eslint-disable no-console */
-  stream.on('data', console.log)
-  stream.on('err', console.error)
-  stream.on('end', () => console.log(`${name} Stream closed`))
-}
-
-export default async () => {
-  // unpack gz to sql
-  // execSync('scripts/extrac', (err, stdout, stderr) => {
-
-  const network = await new Network().start()
-  const postgresContainer = initializePostgres(network)
-  const postgresStarted = await postgresContainer.start()
-  const liquibaseContainer = initializeLiquibase(network)
-  await liquibaseContainer.start()
-
-  process.env.POSTGRES_PORT = postgresStarted.getMappedPort(5432).toString()
-  containers.push(postgresStarted)
-}
-
-function initializeLiquibase(network) {
-  // Pinning Liquibase version for now, we were getting an error with 5.0.0. This should removed later
-  return new GenericContainer('liquibase/liquibase:4.32.0')
-    .withName('liquibase')
-    .withNetwork(network)
-    .withLogConsumer((stream) => log(stream, 'Liquibase'))
-    .withWaitStrategy(
-      Wait.forLogMessage("Liquibase command 'update' was executed successfully")
-    )
-    .withStartupTimeout(120_000)
-    .withCopyDirectoriesToContainer([
-      {
-        source: path.resolve(
-          path.dirname(fileURLToPath(import.meta.url)),
-          '../../../changelog'
-        ),
-        target: '/liquibase/changelog'
-      }
-    ])
-    .withCommand([
-      'update',
-      '--url=jdbc:postgresql://postgis:5432/' + DB_CONFIG.database,
-      '--username=' + DB_CONFIG.user,
-      '--password=' + DB_CONFIG.password,
-      '--changeLogFile=/changelog/db.changelog.xml'
-    ])
-}
-
-function initializePostgres(network) {
-  return new GenericContainer('postgis/postgis:16-3.4')
-    .withName('postgis')
-    .withNetwork(network)
-    .withExposedPorts(5432)
-    .withLogConsumer((stream) => log(stream, 'Postgres'))
-    .withTmpFs(type() === 'Linux' ? { '/var/lib/postgresql/data': '' } : {})
-    .withEnvironment({
-      POSTGRES_USER: DB_CONFIG.user,
-      POSTGRES_PASSWORD: DB_CONFIG.password,
-      POSTGRES_DB: DB_CONFIG.database,
-      POSTGRES_PORT: '5432'
-    })
 }
