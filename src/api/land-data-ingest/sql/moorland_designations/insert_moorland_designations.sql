195926bad565bcc803b2fa89ce14dac407b82446
Andrew Timney <andrew.timney@gmail.com>
2025-11-26 14:20:20 +0000

    use unique id for moorland designations

--- src/api/land-data-ingest/sql/moorland_designations/insert_moorland_designations.sql
+++ src/api/land-data-ingest/sql/moorland_designations/insert_moorland_designations.sql
@@ -1,5 +1,5 @@
-INSERT INTO moorland_designations (lfa_moor_id, name, ref_code, geom, last_updated)
-SELECT LFAMOORID, name, ref_code, geom, CURRENT_TIMESTAMP
+INSERT INTO moorland_designations (id, lfa_moor_id, name, ref_code, geom, last_updated)
+SELECT OBJECTID, LFAMOORID, name, ref_code, geom, CURRENT_TIMESTAMP
 FROM moorland_designations_tmp
 ON CONFLICT (id) 
 DO UPDATE SET
