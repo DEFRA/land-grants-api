780a98f6053a4151734e656d75118eeeadbbd047
Andrew Timney <andrew.timney@gmail.com>
2025-11-26 15:46:38 +0000

    truncate moorland designations on insert

--- src/api/land-data-ingest/sql/moorland_designations/insert_moorland_designations.sql
+++ src/api/land-data-ingest/sql/moorland_designations/insert_moorland_designations.sql
@@ -1,10 +1,5 @@
-INSERT INTO moorland_designations (id, lfa_moor_id, name, ref_code, geom, last_updated)
-SELECT OBJECTID, LFAMOORID, name, ref_code, geom, CURRENT_TIMESTAMP
-FROM moorland_designations_tmp
-ON CONFLICT (id) 
-DO UPDATE SET
-  lfa_moor_id = EXCLUDED.lfa_moor_id,
-  name = EXCLUDED.name,
-  ref_code = EXCLUDED.ref_code,
-  geom = EXCLUDED.geom,
-  last_updated = NOW();
+TRUNCATE TABLE moorland_designations;
+
+INSERT INTO moorland_designations (lfa_moor_id, name, ref_code, geom, last_updated)
+SELECT LFAMOORID, name, ref_code, geom, CURRENT_TIMESTAMP
+FROM moorland_designations_tmp;
