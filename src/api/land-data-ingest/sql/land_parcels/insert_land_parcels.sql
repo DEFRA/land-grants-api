db4d3d363a03a13c2cd0b2f1917326a9e48aa1aa
Nick Froud <nfroud@gmail.com>
2025-12-08 11:52:36 +0000

    switched to parcel_id and sheet_id for de-duping, removing id inserts for land parcels, add ingest id for land parcel insert

--- src/api/land-data-ingest/sql/land_parcels/insert_land_parcels.sql
+++ src/api/land-data-ingest/sql/land_parcels/insert_land_parcels.sql
@@ -1,7 +1,7 @@
-INSERT INTO land_parcels (id, parcel_id, sheet_id, area_sqm, geom, last_updated)
-SELECT ID, PARCEL_ID, SHEET_ID, GEOM_AREA_SQM, geom, LAST_REFRESH_DATE
+INSERT INTO land_parcels (parcel_id, sheet_id, area_sqm, geom, last_updated, ingest_id)
+SELECT PARCEL_ID, SHEET_ID, GEOM_AREA_SQM, geom, LAST_REFRESH_DATE, $1
 FROM land_parcels_tmp
-ON CONFLICT (id) 
+ON CONFLICT (parcel_id, sheet_id) 
 DO UPDATE SET
   area_sqm = EXCLUDED.area_sqm,
   geom = EXCLUDED.geom,
