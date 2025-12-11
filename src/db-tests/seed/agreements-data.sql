5542a8e8c1d4a5b0d8037f34131c027d7bf60a34
andrewkeig <andrew.keig@gmail.com>
2025-12-11 14:03:10 +0000

    re enable db tests

---
+++ src/db-tests/seed/agreements-data.sql
@@ -0,0 +1,37 @@
+TRUNCATE TABLE public.agreements;
+
+INSERT INTO public.agreements(parcel_id, sheet_id, actions)
+VALUES ('7268', 'SD6743', '[{"action_code": "CMOR1", "unit": "ha", "quantity": 10}, {"action_code": "UPL1", "unit": "ha", "quantity": 50}]');
+
+INSERT INTO public.agreements(parcel_id, sheet_id, actions)
+VALUES ('0637', 'SD5665', '[{"action_code": "CMOR1", "unit": "ha", "quantity": 4.47936112}]');
+
+INSERT INTO public.agreements(parcel_id, sheet_id, actions)
+VALUES ('4622', 'SD4963', '[{"action_code": "UPL1", "unit": "ha", "quantity": 1.75055834}]');
+
+INSERT INTO public.agreements(parcel_id, sheet_id, actions)
+VALUES ('8058', 'SD7164', '[{"action_code": "WS1", "unit": "ha", "quantity": 2.53282335}]');
+
+INSERT INTO public.agreements(parcel_id, sheet_id, actions)
+VALUES ('5484', 'SD5253', '[{"action_code": "CNUM2", "unit": "ha", "quantity": 6.96432067}]');
+
+INSERT INTO public.agreements(parcel_id, sheet_id, actions)
+VALUES ('4156', 'SD7948', '[{"action_code": "OP1", "unit": "ha", "quantity": 0.2},{"action_code": "OP2", "unit": "ha", "quantity": 0.2},{"action_code": "OP3", "unit": "ha", "quantity": 0.2}]');
+
+INSERT INTO public.agreements(parcel_id, sheet_id, actions)
+VALUES ('1509', 'SD8447', '[{"action_code": "CHRW1", "unit": "ha", "quantity": 10},{"action_code": "CHRW2", "unit": "ha", "quantity": 8},{"action_code": "CHRW3", "unit": "ha", "quantity": 7}]');
+
+INSERT INTO public.agreements(parcel_id, sheet_id, actions)
+VALUES ('8307', 'SD4957', '[{"action_code": "CHRW1", "unit": "ha", "quantity": 6},{"action_code": "CHRW2", "unit": "ha", "quantity": 4},{"action_code": "CHRW3", "unit": "ha", "quantity": 2},{"action_code": "PRF1", "unit": "ha", "quantity": 2},{"action_code": "PRF2", "unit": "ha", "quantity": 4},{"action_code": "GRH6", "unit": "ha", "quantity": 6},{"action_code": "GRH7", "unit": "ha", "quantity": 4}]');
+
+INSERT INTO public.agreements(parcel_id, sheet_id, actions)
+VALUES ('3264', 'SD8743', '[{"action_code": "UPL1", "unit": "ha", "quantity": 2},{"action_code": "UPL2", "unit": "ha", "quantity": 2},{"action_code": "UPL3", "unit": "ha", "quantity": 1.85360918},{"action_code": "UPL4", "unit": "ha", "quantity": 2}]');
+
+INSERT INTO public.agreements(parcel_id, sheet_id, actions)
+VALUES ('1283', 'SD6441', '[{"action_code": "CMOR1", "unit": "ha", "quantity": 4.59566486},{"action_code": "UPL2", "unit": "ha", "quantity": 1},{"action_code": "UPL3", "unit": "ha", "quantity": 1.59566486},{"action_code": "UPL5", "unit": "ha", "quantity": 1.59566486}]');
+
+INSERT INTO public.agreements(parcel_id, sheet_id, actions)
+VALUES ('8326', 'SD5563', '[{"action_code": "CMOR1", "unit": "ha", "quantity": 8.81196426},{"action_code": "UPL1", "unit": "ha", "quantity": 3},{"action_code": "UPL2", "unit": "ha", "quantity": 2},{"action_code": "UPL3", "unit": "ha", "quantity": 3.8119643}]');
+
+INSERT INTO public.agreements(parcel_id, sheet_id, actions)
+VALUES ('2399', 'SD6943', '[{"action_code": "UPL1", "unit": "ha", "quantity": 3.01241373}]');
