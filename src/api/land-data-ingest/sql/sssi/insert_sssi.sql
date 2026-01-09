798fc11aba341fe1e521b0fb979e8ee1a0192728
Andrew Timney <andrew.timney@gmail.com>
2026-01-09 09:45:37 +0000

    sssi not truncate

--- src/api/land-data-ingest/sql/sssi/insert_sssi.sql
+++ src/api/land-data-ingest/sql/sssi/insert_sssi.sql
@@ -1,3 +1,9 @@
 INSERT INTO sssi (ensis_id, global_id, geom, sssi_name, last_updated, ingest_id)
 SELECT ensis_id, globalid, geom, sssi_name, CURRENT_TIMESTAMP, $1
-FROM sssi_tmp;
+FROM sssi_tmp
+ON CONFLICT (ensis_id, global_id) 
+DO UPDATE SET
+  geom = EXCLUDED.geom,
+  sssi_name = EXCLUDED.sssi_name,
+  last_updated = NOW(),
+  ingest_id = $1;
