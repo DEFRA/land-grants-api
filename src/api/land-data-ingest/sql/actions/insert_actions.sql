c230d20dbe00521223a5ed76ae89d27e8ff8df69
andrewkeig <andrew.keig@gmail.com>
2026-01-12 08:26:09 +0000

    remove id from action ingestion

--- src/api/land-data-ingest/sql/actions/insert_actions.sql
+++ src/api/land-data-ingest/sql/actions/insert_actions.sql
@@ -1,7 +1,7 @@
-INSERT INTO actions (id, code, description, enabled, display, ingest_id, import_date)
-SELECT id, code, description, enabled, display, $1, NOW()
+INSERT INTO actions (code, description, enabled, display, ingest_id, import_date)
+SELECT code, description, enabled, display, $1, NOW()
 FROM actions_tmp
-ON CONFLICT (id) DO UPDATE SET
+ON CONFLICT (code) DO UPDATE SET
   code = EXCLUDED.code,
   description = EXCLUDED.description,
   enabled = EXCLUDED.enabled,
