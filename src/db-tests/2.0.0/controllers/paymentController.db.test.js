ef8f73c1bcf313f66a439044b1b483b57bb2cc5a
andrewkeig <andrew.keig@gmail.com>
2026-02-02 11:43:39 +0000

    Fix remaining tests

--- src/db-tests/2.0.0/controllers/paymentController.db.test.js
+++ src/db-tests/2.0.0/controllers/paymentController.db.test.js
@@ -1,27 +1,115 @@
 /* eslint-disable no-console */
-import { vi } from 'vitest'
-import { PaymentsCalculateControllerV2 as PaymentsCalculateController } from '~/src/api/payment/controllers/2.0.0/payment-calculate.controller.js'
+import { PaymentsCalculateControllerV1 as PaymentsCalculateController } from '~/src/api/payment/controllers/1.0.0/payment-calculate.controller.js'
+import { getEnabledActions } from '~/src/api/actions/queries/1.0.0/getActions.query.js'
 import { connectToTestDatbase } from '~/src/db-tests/setup/postgres.js'
 import { createResponseCapture } from '../../setup/utils.js'
 import { getPaymentCalculationFixtures } from '../../setup/getPaymentCalculationFixtures.js'
 import { validateRequest } from '~/src/api/application/validation/application.validation.js'
-import { logger } from '~/src/db-tests/setup/testLogger.js'
+import { vi } from 'vitest'
 
 vi.mock('~/src/api/application/validation/application.validation.js')
+vi.mock('~/src/api/actions/queries/1.0.0/getActions.query.js')
 
 const mockValidateRequest = validateRequest
+const mockGetEnabledActions = getEnabledActions
+const landCoverClassCodes = [
+  '130',
+  '240',
+  '250',
+  '270',
+  '280',
+  '300',
+  '330',
+  '580',
+  '590',
+  '620',
+  '640',
+  '650'
+]
 
-describe('Payment Controller 2.0.0', () => {
-  let connection
+const actions = [
+  {
+    enabled: true,
+    code: 'CMOR1',
+    payment: {
+      ratePerUnitGbp: 10.6,
+      ratePerAgreementPerYearGbp: 272
+    },
+    applicationUnitOfMeasurement: 'ha',
+    durationYears: 3,
+    landCoverClassCodes,
+    startDate: '2025-01-01',
+    version: 1
+  },
+  {
+    enabled: true,
+    code: 'UPL1',
+    payment: {
+      ratePerUnitGbp: 20
+    },
+    applicationUnitOfMeasurement: 'ha',
+    durationYears: 3,
+    landCoverClassCodes,
+    startDate: '2025-01-01',
+    version: 1
+  },
+  {
+    enabled: true,
+    code: 'UPL2',
+    payment: {
+      ratePerUnitGbp: 53
+    },
+    applicationUnitOfMeasurement: 'ha',
+    durationYears: 3,
+    landCoverClassCodes,
+    startDate: '2025-01-01',
+    version: 1
+  },
+  {
+    enabled: true,
+    code: 'UPL3',
+    payment: {
+      ratePerUnitGbp: 66
+    },
+    applicationUnitOfMeasurement: 'ha',
+    durationYears: 3,
+    landCoverClassCodes,
+    startDate: '2025-01-01',
+    version: 1
+  },
+  {
+    enabled: true,
+    code: 'CSAM1',
+    payment: {
+      ratePerUnitGbp: 6,
+      ratePerAgreementPerYearGbp: 97
+    },
+    applicationUnitOfMeasurement: 'ha',
+    durationYears: 3,
+    landCoverClassCodes,
+    startDate: '2025-01-01',
+    version: 1
+  }
+]
+
+describe('Payment Controller', () => {
+  let logger, connection
   const fixtures = getPaymentCalculationFixtures()
   const { h, getResponse } = createResponseCapture()
 
   beforeAll(() => {
+    logger = {
+      info: vi.fn(),
+      error: vi.fn(),
+      warn: vi.fn(),
+      debug: vi.fn()
+    }
     connection = connectToTestDatbase()
   })
 
   beforeEach(() => {
     mockValidateRequest.mockResolvedValue([])
+    mockGetEnabledActions.mockResolvedValue(actions)
   })
 
   afterAll(async () => {
@@ -29,7 +117,7 @@ describe('Payment Controller 2.0.0', () => {
   })
 
   test.each(fixtures)(
-    `%p`,
+    `%s`,
     async (
       name,
       {
@@ -74,12 +162,11 @@ describe('Payment Controller 2.0.0', () => {
       expect(secondPayment.totalPaymentPence).toEqual(
         Number(expectedPaymentOtherQuarters)
       )
-
       expect(payment.agreementStartDate).toEqual(expectedStartDate)
       expect(payment.agreementEndDate).toEqual(expectedEndDate)
       expect(payment.annualTotalPence).toEqual(Number(expectedPaymentAnnual))
       expect(firstPayment.paymentDate).toEqual(expectedFirstPaymentDate)
-      expect(payment.parcelItems[1].version).toBe('1.0.0')
+      expect(payment.parcelItems[1].version).toBe(1)
     }
   )
 })
--- vitest.db.config.js
+++ vitest.db.config.js
@@ -13,32 +13,7 @@ export default defineConfig({
   test: {
     ...unitConfig.test,
     name: 'database',
-    include: [
-      // Controllers
-      // 'src/db-tests/1.0.0/controllers/cdpUploaderCallback.db.test.js',
-      // 'src/db-tests/1.0.0/controllers/applicationValidationController.db.test.js',
-      // 'src/db-tests/1.0.0/controllers/paymentController.db.test.js'
-      // 'src/db-tests/1.0.0/controllers/getParcels.db.test.js'
-      // 'src/db-tests/1.0.0/controllers/parcelsController.db.test.js'
-      // // Mutations
-      // 'src/db-tests/1.0.0/mutations/saveApplicationValidationRun.db.test.js',
-      // 'src/db-tests/1.0.0/mutations/saveApplication.db.test.js',
-      // // Queries
-      // 'src/db-tests/1.0.0/queries/getActions.db.test.js',
-      // 'src/db-tests/1.0.0/queries/getAgreementsForParcel.query.test.js',
-      // 'src/db-tests/1.0.0/queries/getApplicationValidationRun.db.test.js',
-      // 'src/db-tests/1.0.0/queries/getApplicationValidationRuns.db.test.js',
-      // 'src/db-tests/1.0.0/queries/getCompatibilityMatrix.db.test.js',
-      // 'src/db-tests/1.0.0/queries/getLandCoverDefinitions.db.test.js',
-      // 'src/db-tests/1.0.0/queries/getLandCoversForAction.db.test.js',
-      // 'src/db-tests/1.0.0/queries/getParcelAvailableArea.db.test.js'
-      // 'src/db-tests/1.0.0/queries/getMoorlandInterceptPercentage.db.test.js'
-      // 'src/db-tests/1.0.0/queries/getDataLayerInterceptPercentage.db.test.js'
-      // Services
-      // 'src/db-tests/1.0.0/services/getPaymentCalculation.scenarios.db.test.js'
-      // todo:
-      'src/db-tests/1.0.0/services/getAvailableAreaForAction.scenarios.db.test.js'
-    ],
+    include: ['src/db-tests/**/?(*.)+(spec|test).[jt]s?(x)'],
     globalSetup: [resolve(__dirname, 'vitest.db.setup.js')],
     globalTeardown: [resolve(__dirname, 'vitest.db.teardown.js')],
     coverage: {
