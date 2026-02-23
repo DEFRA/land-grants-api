340e947f9ef3faa6de2fe50056b259338a72a9d9
Andrew Timney <andrew.timney@gmail.com>
2026-02-23 15:27:29 +0000

    include type in data layer upsert check

--- src/features/land-data-ingest/sql/sssi/insert_sssi.sql
+++ src/features/land-data-ingest/sql/sssi/insert_sssi.sql
@@ -1,4 +1,4 @@
-INSERT INTO data_layer (source_id, geom, name, metadata, data_layer_type_id, last_updated, ingest_id)
+INSERT INTO data_layer (source_id, geom, "name", metadata, data_layer_type_id, last_updated, ingest_id)
 SELECT 
     globalid, 
     geom, 
@@ -11,10 +11,10 @@ SELECT
     TO_DATE(modified,'YYYY/MM/DD'), 
     $1
 FROM sssi_tmp
-ON CONFLICT (source_id) 
+ON CONFLICT (source_id, data_layer_type_id) 
 DO UPDATE SET
   geom = EXCLUDED.geom,
-  name = EXCLUDED.name,
+  "name" = EXCLUDED."name",
   metadata = EXCLUDED.metadata,
   last_updated = EXCLUDED.last_updated,
   ingest_id = $1;
