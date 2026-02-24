94480b665ee1c9fdbb0e2c70f5f3e37446787955
Andrew Timney <andrew.timney@gmail.com>
2026-02-24 09:11:14 +0000

    SFIR-1052: add case-management-adapter contract test (#487)
    * add case-management-adapter contract test
    
    * formatting

--- src/tests/contract-tests/provider.test.js
+++ src/tests/contract-tests/provider.test.js
@@ -9,6 +9,7 @@ import { mockActionConfig } from '~/src/features/actions/fixtures/index.js'
 import { parcel } from '~/src/features/parcel/index.js'
 import { payments } from '~/src/features/payment/index.js'
 import { application } from '~/src/features/application/index.js'
+import { caseManagementAdapter } from '~/src/features/case-management-adapter/index.js'
 import {
   getAvailableAreaDataRequirements,
   getAvailableAreaForAction
@@ -19,6 +20,9 @@ import { getEnabledActions } from '~/src/features/actions/queries/getActions.que
 import { getActionsByLatestVersion } from '~/src/features/actions/queries/2.0.0/getActionsByLatestVersion.query.js'
 import { saveApplication } from '~/src/features/application/mutations/saveApplication.mutation.js'
 import { getLatestVersion } from './git.js'
+import { getApplicationValidationRun } from '~/src/features/application/queries/getApplicationValidationRun.query.js'
+import { applicationValidationRunToCaseManagement } from '~/src/features/case-management-adapter/transformers/application-validation.transformer.js'
+import { validateApplication } from '~/src/features/application/service/application-validation.service.js'
 
 vi.mock('~/src/features/parcel/queries/getLandData.query.js')
 vi.mock('~/src/features/actions/queries/getActions.query.js')
@@ -32,6 +36,13 @@ vi.mock(
   '~/src/features/land-cover-codes/queries/getLandCoversForActions.query.js'
 )
 vi.mock('~/src/features/agreements/queries/getAgreementsForParcel.query.js')
+vi.mock(
+  '~/src/features/application/queries/getApplicationValidationRun.query.js'
+)
+vi.mock(
+  '~/src/features/case-management-adapter/transformers/application-validation.transformer.js'
+)
+vi.mock('~/src/features/application/service/application-validation.service.js')
 
 const mockGetLandData = getLandData
 const mockGetActionsByLatestVersion = getActionsByLatestVersion
@@ -42,6 +53,10 @@ const mockGetAgreementsForParcel = getAgreementsForParcel
 const mockGetAvailableAreaDataRequirements = getAvailableAreaDataRequirements
 const mockSaveApplication = saveApplication
 const mockCompatibilityCheckFn = vi.fn()
+const mockGetApplicationValidationRun = getApplicationValidationRun
+const mockApplicationValidationRunToCaseManagement =
+  applicationValidationRunToCaseManagement
+const mockValidateApplication = validateApplication
 
 const mockAvailableAreaResult = {
   stacks: [],
@@ -49,6 +64,47 @@ const mockAvailableAreaResult = {
   totalValidLandCoverSqm: 300,
   availableAreaSqm: 300,
   availableAreaHectares: 0.03
+}
+
+// eslint-disable-next-line
+const mockGetApplicationValidationRunResult = async (logger, db, id) => {
+  if (id === 999) {
+    return null
+  }
+  return {
+    id,
+    sbi: 123456,
+    crn: '123456',
+    application_id: 1,
+    validation_run_id: 1,
+    data: {
+      application_id: 1
+    }
+  }
+}
+
+const mockValidationRunToCaseManagementResult = [
+  {
+    component: 'heading',
+    text: 'Land parcel rules checks',
+    level: 2,
+    id: '1'
+  },
+  {
+    component: 'heading',
+    text: 'Parcel ID: SD6743 8083 checks',
+    level: 3,
+    id: undefined
+  }
+]
+
+const mockValidateApplicationResult = {
+  validationErrors: [],
+  applicationData: {
+    hasPassed: true,
+    date: '2026-02-23T11:09:46.263Z'
+  },
+  applicationValidationRunId: 1
 }
 
 dotenv.config()
@@ -99,6 +155,13 @@ const pactVerifierOptions = async () => {
       mockGetAvailableAreaForAction.mockReturnValue(mockAvailableAreaResult)
       mockGetAgreementsForParcel.mockResolvedValue([])
       mockSaveApplication.mockResolvedValue(251)
+      mockGetApplicationValidationRun.mockImplementation(
+        mockGetApplicationValidationRunResult
+      )
+      mockApplicationValidationRunToCaseManagement.mockReturnValue(
+        mockValidationRunToCaseManagementResult
+      )
+      mockValidateApplication.mockResolvedValue(mockValidateApplicationResult)
     },
 
     afterEach: () => {
@@ -116,7 +179,12 @@ describe('Pact Verification', () => {
       connect: vi.fn(),
       query: vi.fn()
     })
-    await server.register([parcel, payments, application])
+    await server.register([
+      parcel,
+      payments,
+      application,
+      caseManagementAdapter
+    ])
     await server.initialize()
     await server.start()
   })
