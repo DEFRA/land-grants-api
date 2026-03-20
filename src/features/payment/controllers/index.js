4a4c3e4a532540d4e95a8484452106673059dfc5
Andrew Timney <andrew.timney@gmail.com>
2026-03-20 15:43:03 +0000

    formatting

--- src/features/payment/controllers/index.js
+++ src/features/payment/controllers/index.js
@@ -2,4 +2,8 @@ import { PaymentsCalculateControllerV1 } from './1.0.0/payment-calculate.control
 import { PaymentsCalculateControllerV2 } from './2.0.0/payment-calculate.controller.js'
 import { PaymentsCalculateWMPControllerV2 } from './2.0.0/payment-calculate-wmp.controller.js'
 
-export { PaymentsCalculateControllerV1, PaymentsCalculateControllerV2, PaymentsCalculateWMPControllerV2 }
+export {
+  PaymentsCalculateControllerV1,
+  PaymentsCalculateControllerV2,
+  PaymentsCalculateWMPControllerV2
+}
--- src/features/payment/schema/2.0.0/payment-calculate-wmp.controller.js
+++ src/features/payment/schema/2.0.0/payment-calculate-wmp.controller.js
@@ -1,10 +1,10 @@
 import Joi from 'joi'
 
 export const paymentCalculateWMPResponseSchemaV2 = Joi.object({
-    result: Joi.string().required(),
+  result: Joi.string().required()
 })
 
 export const paymentCalculateWMPSchemaV2 = Joi.object({
-    parcelIds: Joi.array().items(Joi.string()).required(),
-    areaHectares: Joi.number().positive().required(),
+  parcelIds: Joi.array().items(Joi.string()).required(),
+  areaHectares: Joi.number().positive().required()
 })
