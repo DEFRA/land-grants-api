f616fcc1abc93fba2565a45b618efa04544e9b33
andrewkeig <andrew.keig@gmail.com>
2026-03-30 14:21:07 +0100

    fix merge issues

--- src/features/woodland-management/service/wmp-payment-calculate.service.js
+++ src/features/woodland-management/service/wmp-payment-calculate.service.js
@@ -33,7 +33,7 @@ export const executeRulesForPaymentCalculationWMP = (
 /**
  * Sums the area of all parcels to produce the total land area.
  * @param {Array<LandParcelDb | null>} parcels - The land parcels
-  * @returns {number} The total land area in square metres
+ * @returns {number} The total land area in square metres
  */
 export const sumTotalLandAreaSqm = (parcels) => {
   return parcels.reduce((acc, parcel) => acc + (parcel?.area ?? 0), 0)
