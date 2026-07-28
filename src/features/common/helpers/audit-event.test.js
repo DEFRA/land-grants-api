90efe8c56e840490516b5f9d79fcc7fa7d09bef9
PauloPortugal <paulo.from.portugal@gmail.com>
2026-07-28 12:34:52 +0100

    GSPS-497 Only log the full audit payload in case of failure

--- src/features/common/helpers/audit-event.test.js
+++ src/features/common/helpers/audit-event.test.js
@@ -240,10 +240,8 @@ describe('auditEvent', () => {
 
     await auditEvent(UNMAPPED_EVENT, context)
 
-    const publishedMessage = mockSend.mock.calls[0][0].input.Message
-
     expect(mockLogger.info).toHaveBeenCalledWith(
-      `Audit event successfully published: ${UNMAPPED_EVENT} ${publishedMessage}`
+      `Audit event successfully published: ${UNMAPPED_EVENT}`
     )
   })
 })
