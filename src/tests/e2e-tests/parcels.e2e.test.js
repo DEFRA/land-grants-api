5bdaa634c6c34ac66d1eba2ff0335487e84f89ef
David Capilla <david.capilla@gmail.com>
2026-08-05 11:45:21 +0100

    TGC-1539 - Add availability and guidanceUrl for actions

--- src/tests/e2e-tests/parcels.e2e.test.js
+++ src/tests/e2e-tests/parcels.e2e.test.js
@@ -19,6 +19,10 @@ describe('Parcels Endpoints', () => {
         }
       })
 
+      console.log('Status:', response.status)
+      console.log('Response:', JSON.stringify(response.data, null, 2))
+
+
       expect(response.status).toBe(200)
       expect(response.data.message).toBe('success')
       expect(response.data.parcels).toHaveLength(1)
