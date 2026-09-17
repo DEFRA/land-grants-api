106b60540d2a084aeaf2577167fae29b740972e9
Rob Moore <giftiger.wunsch@gmail.com>
2026-09-17 09:47:59 +0100

    GSPS-621: Only call the DAL once per SBI
    N.B. part one of two -- currently refactoring the parcels API route but
    not the validation routes to keep this change granular.
    
    Currently the agreement fetching code lives within getActionsForParcel,
    which means we're hitting both the db and the DAL for agreements once
    for each parcel in the request -- which could well be 100 parcels.
    
    Update the DAL code so that instead of filtering the result from the DAL
    by parcelId + sheetId, it groups results by that key and provides a
    summary for the whole SBI.
    
    Refactor the postgres query to also fetch agreement data from all
    requested parcels at once and present it the same way.
    
    Clean up tests while I'm here, removing tests which cover behaviour we
    should NOT allow, such as passing wrong types, removing highly fragile
    and useless assertions like the exact wording of error messages or
    phrasings of queries, and splitting some extremely-repeated values out
    into reusable fixtures and consts.
    
    Refactor the getAgreements call to be outside the getActionsForParcel
    loop and add on the agreement data.
    
    We have similar optimisation issues around SSSI and HEFER queries which
    have already proven problematic in the past, so a future improvement
    would be to treat those queries similarly.

--- src/features/application/service/land-parcel-validation.service.js
+++ src/features/application/service/land-parcel-validation.service.js
@@ -1,4 +1,5 @@
 import { getAgreements } from '~/src/features/agreements/repo.js'
+import { expiredActionsFilter } from '~/src/features/agreements/transformers/filters.js'
 import { validateLandAction } from './action-validation.service.js'
 
 /**
@@ -24,17 +25,17 @@ export const validateLandParcelActions = async (
     throw new Error('Unable to validate land parcel actions')
   }
 
-  // Get agreements and filter them to only area-based actions, as only
-  // these should be used for Available Area Calculations
-  const agreements = await getAgreements(
+  const allAgreements = await getAgreements(
     sbi,
-    landAction.sheetId,
-    landAction.parcelId,
+    [[landAction.parcelId, landAction.sheetId]],
     defraIdToken,
     request.server.postgresDb,
-    request.logger,
-    referenceDate
+    request.logger
   )
+
+  const agreements = (
+    allAgreements[`${landAction.parcelId}-${landAction.sheetId}`] || []
+  ).filter((a) => expiredActionsFilter(a, referenceDate))
 
   const actionResults = await Promise.all(
     landAction.actions.map(async (action) => {
